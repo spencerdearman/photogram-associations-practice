6f3b6de74ac3b2d5792208f3a4b94ceff04451b5
Spencer Dearman <121642493+spencerdearman@users.noreply.github.com>
2024-11-18 04:21:11 +0000

    done

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -17,11 +17,15 @@ class FollowRequest < ApplicationRecord
     :uniqueness => { :scope => [:sender_id] }
   })
 
+
   # Association accessor methods to define:
   
   ## Direct associations
 
   # FollowRequest#sender: returns a row from the users table associated to this follow request by the sender_id column
+  belongs_to(:sender, class_name: "User", foreign_key: "sender_id")
 
   # FollowRequest#recipient: returns a row from the users table associated to this follow request by the recipient_id column
+  belongs_to(:recipient, class_name: "User", foreign_key: "recipient_id")
+  
 end
